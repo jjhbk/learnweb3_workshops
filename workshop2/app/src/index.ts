8860522a14c28821f339472e19b13bd6fec7b654
jjhbk <jp@ph.iitr.ac.in>
2023-10-26 19:30:40 +0530

    working code

--- workshop2/app/src/index.ts
+++ workshop2/app/src/index.ts
@@ -1,34 +1,49 @@
 // XXX even though ethers is not used in the code below, it's very likely
 // it will be used by any DApp, so we are already including it here
 const { ethers } = require("ethers");
-import { encodeFunctionData, getAddress } from "viem";
+import { encodeFunctionData, hexToString, getAddress, stringToHex } from "viem";
 import { storageabi } from "./contracts";
+
 const rollup_server = process.env.ROLLUP_HTTP_SERVER_URL;
 console.log("HTTP rollup_server url is " + rollup_server);
-const storage_address = getAddress(
-  "0xf1a0B31f4647e4F403e420AD90E7a1fbb53c4f13"
+const storage_contract_address = getAddress(
+  "0xc6e7DF5E7b4f2A278906862b61205850344D4e7d"
 );
+let Storage_value = BigInt(0);
 //for goerli 0xf1a0B31f4647e4F403e420AD90E7a1fbb53c4f13
 // for localhost   "0xc6e7DF5E7b4f2A278906862b61205850344D4e7d"
+
 async function handle_advance(data: any) {
   console.log("Received advance request data " + JSON.stringify(data));
-  const payload = data["payload"];
+  const payload = data.payload;
+  let JSONpayload: any = {};
   try {
-    const payloadStr = ethers.utils.toUtf8String(payload);
-    console.log(`Adding notice "${payloadStr}"`);
+    const payloadStr = hexToString(payload);
+    JSONpayload = JSON.parse(payloadStr);
+    console.log(`received request "${JSON.stringify(JSONpayload)}"`);
   } catch (e) {
     console.log(`Adding notice with binary value "${payload}"`);
+    await fetch(rollup_server + "/report", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ payload: payload, error: e }),
+    });
+    return "accept";
   }
   let advance_req;
 
-  if (payload == "0x766f7563686572") {
+  if (JSONpayload.method === "store_value") {
+    console.log("storing the new value");
+
     const call = encodeFunctionData({
       abi: storageabi,
       functionName: "store",
-      args: [123124554],
+      args: [JSONpayload.value],
     });
     let voucher = {
-      destination: storage_address, // dapp Address
+      destination: storage_contract_address, // dapp Address
       payload: call,
     };
     console.log(voucher);
@@ -40,13 +55,16 @@ async function handle_advance(data: any) {
       body: JSON.stringify(voucher),
     });
     console.log("starting a voucher");
-  } else if (payload == "0x6e6f74696365") {
+  } else if (JSONpayload.method === "get_value") {
+    console.log("fetching the stored value");
+    const result = JSON.stringify({ value: Storage_value.toString() });
+    const hexresult = stringToHex(result);
     advance_req = await fetch(rollup_server + "/notice", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ payload }),
+      body: JSON.stringify({ payload: hexresult }),
     });
   } else {
     advance_req = await fetch(rollup_server + "/report", {
@@ -57,7 +75,6 @@ async function handle_advance(data: any) {
       body: JSON.stringify({ payload }),
     });
   }
-
   const json = await advance_req.json();
   console.log(
     "Received  status " +
@@ -70,14 +87,27 @@ async function handle_advance(data: any) {
 
 async function handle_inspect(data: any) {
   console.log("Received inspect request data " + JSON.stringify(data));
-  const payload = data["payload"];
+  const payload = data.payload;
+  let inspect_req;
   try {
-    const payloadStr = ethers.utils.toUtf8String(payload);
-    console.log(`Adding report "${payloadStr}"`);
+    const payloadStr = hexToString(payload);
+    console.log("received inspect request with payload", payloadStr);
+    if (payloadStr === "storage") {
+      console.log("fetching the stored value result", Storage_value);
+      inspect_req = await fetch(rollup_server + "/report", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ value: Storage_value }),
+      });
+      console.log("Adding report with" + inspect_req.status);
+      return "accept";
+    }
   } catch (e) {
     console.log(`Adding report with binary value "${payload}"`);
   }
-  const inspect_req = await fetch(rollup_server + "/report", {
+  inspect_req = await fetch(rollup_server + "/report", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -111,8 +141,8 @@ var finish = { status: "accept" };
       console.log("No pending rollup request, trying again");
     } else {
       const rollup_req = await finish_req.json();
-      var handler = handlers[rollup_req["request_type"]];
-      finish["status"] = await handler(rollup_req["data"]);
+      var handler = handlers[rollup_req.request_type];
+      finish.status = await handler(rollup_req.data);
     }
   }
 })();
